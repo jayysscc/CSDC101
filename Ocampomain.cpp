cb99b896427228c9d28f95e4192b8c0ecda0fb7f
jayysscc <johnclarenceocampo1020@gmail.com>
2025-08-08 14:44:08 +0800

    Create Ocampomain.cpp

---
+++ Ocampomain.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+ int main (){
+     int num1, num2, sum;
+     
+     cout<< "Enter a Number "; //put a number any number 
+     cin>> num1;            // num 1 
+     cout<<"Enter Another Number ";   // put another number 
+     cin>>num2;
+     sum = num1 + num2;       // random AHAHh 
+     cout<< "the sum of the twi numbers"<<sum; // 1+1
+     
+     return 0;
+ }   
+
+  
+   
